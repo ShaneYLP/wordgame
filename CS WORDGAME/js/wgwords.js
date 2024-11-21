9d0637c947e9cc7c0074fb4565bdfa1ae93e5d08
ShaneYLP <153744151+ShaneYLP@users.noreply.github.com>
2024-11-21 08:49:40 -0500

    Add files via upload

---
+++ CS WORDGAME/js/wgwords.js
@@ -0,0 +1,35 @@
+//Words and hints for the wordgame
+const wordList = [
+    {
+        word: "antivirus",
+        hint: "Software that identifies online threats"
+    },
+    {
+        word: "blacklist",
+        hint: "A tool used to block websites and IP Adresses"
+    },
+    {
+        word: "firewall",
+        hint: "Prevents intruders from breaching network secruity"
+    },
+    {
+        word: "vpn",
+        hint: "Enables users to browse the internet anonymously"
+    },
+    {
+        word: "malware",
+        hint: "Malicious software designed to damage a device"
+    },
+    {
+        word: "phishing",
+        hint: "malicious email that asks users to provide personal info"
+    },
+    {
+        word: "router",
+        hint: "Connects devices to internet"
+    },
+    {
+        word: "virus",
+        hint: "Malware used to infect an entire device"
+    },
+]
